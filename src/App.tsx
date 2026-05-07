e8bc9272dc0765db2010841d982cd2ca7dc6f5c7
kakarotr <18250331338@139.com>
2026-05-07 16:33:06 +0800

    input、input-group、select组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { doc } from './lib/md-content'
-import { MarkdownContent } from './components/markdown/markdown-content'
-import { Input } from './components/ui/input'
+import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem, SelectGroup } from './components/ui/select'
+
+const items = [
+  { label: 'Select a fruit', value: null },
+  { label: 'Apple', value: 'apple' },
+  { label: 'Banana', value: 'banana' },
+  { label: 'Blueberry', value: 'blueberry' },
+  { label: 'Grapes', value: 'grapes' },
+  { label: 'Pineapple', value: 'pineapple' },
+]
+
 
 export function App() {
   return (
     <div className='flex justify-center items-center size-full'>
       <div className='w-96'>
-        <Input/>
+        <Select items={items} >
+          <SelectTrigger className={'w-full max-w-48'}>
+            <SelectValue/>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {items.map((item) => (
+                <SelectItem key={item.value} value={item.value}>
+                  {item.label}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
       {/* <MarkdownContent className='mx-auto' content={doc}/> */}
     </div>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,15 +1,15 @@
-import * as React from "react"
-import { Input as InputPrimitive } from "@base-ui/react/input"
+import * as React from 'react'
+import { Input as InputPrimitive } from '@base-ui/react/input'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
   return (
     <InputPrimitive
       type={type}
-      data-slot="input"
+      data-slot='input'
       className={cn(
-        "h-8 w-full min-w-0 rounded-lg border border-input bg-transparent px-2.5 py-1 text-base transition outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground hover:border-primary focus-visible:border focus-visible:border-primary focus-visible:[box-shadow:0_0_0_2px_color-mix(in_oklch,var(--primary)_10%,transparent)] disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+        'h-8 w-full min-w-0 rounded-lg border border-input bg-transparent px-2.5 py-1 text-base transition outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground hover:border-primary focus-visible:border focus-visible:border-primary focus-visible:ring-primary disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
         className
       )}
       {...props}
