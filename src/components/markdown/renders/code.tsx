c46ad766385359fb20297209c224b79ffd039c06
kakarotr <18250331338@139.com>
2026-05-07 21:20:45 +0800

    移除无用的依赖

--- src/components/markdown/renders/code.tsx
+++
@@ -1,5 +0,0 @@
-import type { ComponentPropsWithoutRef } from 'react'
-
-export function CodeRenderer({ children }: ComponentPropsWithoutRef<'code'>) {
-  return <code>{children}</code>
-}
--- src/components/markdown/renders/index.ts
+++
@@ -1,7 +0,0 @@
-import { CodeRenderer } from './code'
-import { PreRenderer } from './pre'
-
-export const componentMap = {
-  code: CodeRenderer,
-  pre: PreRenderer
-}
